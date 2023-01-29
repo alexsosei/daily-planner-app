b294a01245055b246414825690945bc9186e9bfc
[alexander osei] <[valid.alexsosei@icloud.com]>
2023-01-29 08:20:22 +0000

    more js

--- Assets/dp.js
+++ Assets/dp.js
@@ -4,4 +4,9 @@ function getLocalStorage(key) {
     $('#text${key}');
  }
 }
+$( document ).ready(function () {
+    $("#currentDay").text(moment().format("dddd,MMMM Do"));
+    for (let i = 9; i< 18; i++) {
 
+// create a row
+var row = $(`<div data-time=${i} id='${i}' class="row">`);
